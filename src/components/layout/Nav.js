732310fd179a22112dd0b46067a5727850c6f60b
Lucas Visser <lucasnvp@gmail.com>
2017-11-16 11:14:14 -0300

    Mejora en la forma de los botones

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,20 +4,23 @@ import { Link } from 'react-router-dom';
 class Nav extends Component {
     render() {
         return(
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <div class="collapse navbar-collapse" id="navbarColor01">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <Link to="/">Register</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link to="/">Login</Link>
-                            </li>
-                        </ul>
+            <nav>
+                <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+                    <div class="container">
+                        <Link to="/" class="navbar-brand">Home</Link>
+                        <div class="navbar-collapse collapse show" id="navbarResponsive">
+                            <ul class="nav navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <Link to="/" class="nav-link">Register</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link to="/" class="nav-link">Login</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         );
     }
 
